21e2373f65cf307ab01e53fec90fe088941e989c
Pablo Aguiar <scorphus@gmail.com>
2015-01-18 23:55:17 -0200

    Add metric to measure time spent on a given block
    README structure is adjusted to accommodate documentation for new
    features. Also, tests are improved and cover all the new code.
    
    Thanks @aflag for most valuable tips!

--- tests/acceptance/test_measure.py
+++ tests/acceptance/test_measure.py
@@ -11,7 +11,7 @@ class MeasureTestCase(UDPServerTestCase):
         super(MeasureTestCase, self).setUp()
         self.measure = Measure('myclient', ('127.0.0.1', 1984))
 
-    def test_recieve_message_correctly_over_network(self):
+    def test_recieve_count_message_correctly_over_network(self):
         self.measure.count('mymetric')
         self.wait_for(lambda: len(self.messages))
         self.assertEqual(len(self.messages), 1)
@@ -24,17 +24,52 @@ class MeasureTestCase(UDPServerTestCase):
         }
         self.assertDictEqual(message, expected_message)
 
-    def test_must_not_hang_if_server_is_up(self):
+    def test_recieve_time_message_correctly_over_network(self):
+        with self.measure.time('mymetric'):
+            pass
+        self.wait_for(lambda: len(self.messages))
+        self.assertEqual(len(self.messages), 1)
+
+        message = json.loads(self.messages[0])
+        self.assertIn('client', message)
+        self.assertEqual(message['client'], 'myclient')
+        self.assertIn('metric', message)
+        self.assertEqual(message['metric'], 'mymetric')
+        self.assertIn('time', message)
+        self.assertIsInstance(message['time'], float)
+        self.assertGreater(message['time'], 0)
+        self.assertIn('error_type', message)
+        self.assertEqual(message['error_type'], '')
+        self.assertIn('error_value', message)
+        self.assertEqual(message['error_value'], '')
+
+    def test_count_must_not_hang_if_server_is_up(self):
         p = Process(target=lambda: self.measure.count('mymetric'))
         p.start()
         p.join(0.3)
         self.assertFalse(p.is_alive())
         p.terminate()
 
-    def test_must_not_hang_if_server_is_down(self):
+    def test_time_must_not_hang_if_server_is_up(self):
+        p = Process(target=lambda: self.measure.time('mymetric'))
+        p.start()
+        p.join(0.3)
+        self.assertFalse(p.is_alive())
+        p.terminate()
+
+    def test_count_must_not_hang_if_server_is_down(self):
         self.__class__.stop_server()
         time.sleep(0.5)
         p = Process(target=lambda: self.measure.count('mymetric'))
+        p.start()
+        p.join(0.3)
+        self.assertFalse(p.is_alive())
+        p.terminate()
+
+    def test_time_must_not_hang_if_server_is_down(self):
+        self.__class__.stop_server()
+        time.sleep(0.5)
+        p = Process(target=lambda: self.measure.time('mymetric'))
         p.start()
         p.join(0.3)
         self.assertFalse(p.is_alive())
--- tests/unit/test_measure.py
+++ tests/unit/test_measure.py
@@ -9,9 +9,6 @@ import socket
 
 class MeasureTestCase(TestCase):
 
-    def setUp(self):
-        self.measure = Measure('myclient', ('localhost', 1984))
-
     def test_must_create_a_measure_object_with_correct_attributes(self):
         measure = Measure('myclient', ('localhost', 1984))
         self.assertEqual(measure.client, 'myclient')
@@ -19,6 +16,15 @@ class MeasureTestCase(TestCase):
         self.assertEqual(measure.socket.type, socket.SOCK_DGRAM)
         # socket is non-blocking
         self.assertEqual(measure.socket.gettimeout(), 0.0)
+
+
+class BaseMeasureTestCase(TestCase):
+
+    def setUp(self):
+        self.measure = Measure('myclient', ('localhost', 1984))
+
+
+class MeasureCountTestCase(BaseMeasureTestCase):
 
     @patch('socket.socket.sendto')
     def test_must_send_a_packet_to_correct_address(self, mock_sendto):
@@ -91,3 +97,134 @@ class MeasureTestCase(TestCase):
     def test_must_log_socket_error(self, mock_warn, mock_sendto):
         self.measure.count('mymetric')
         mock_warn.assert_called_once_with('Error on sendto. [Errno 80]')
+
+
+class MeasureTimeTestCase(BaseMeasureTestCase):
+
+    @patch('socket.socket.sendto')
+    def test_must_send_a_packet_to_correct_address(self, mock_sendto):
+        with self.measure.time('mymetric'):
+            pass
+
+        self.assertEqual(mock_sendto.call_count, 1)
+        self.assertEqual(mock_sendto.call_args[0][1], ('localhost', 1984))
+
+    @patch('socket.socket.sendto')
+    def test_must_send_packet_with_time_spent(self, mock_sendto):
+        with self.measure.time('mymetric'):
+            pass
+
+        self.assertEqual(mock_sendto.call_count, 1)
+        message = json.loads(mock_sendto.call_args[0][0])
+        self.assertEqual(len(message), 5)
+        self.assertIn('client', message)
+        self.assertEqual(message['client'], 'myclient')
+        self.assertIn('metric', message)
+        self.assertEqual(message['metric'], 'mymetric')
+        self.assertIn('time', message)
+        self.assertIsInstance(message['time'], float)
+        self.assertGreater(message['time'], 0)
+        self.assertIn('error_type', message)
+        self.assertEqual(message['error_type'], '')
+        self.assertIn('error_value', message)
+        self.assertEqual(message['error_value'], '')
+
+    @patch('socket.socket.sendto')
+    def test_must_send_packet_with_dimensions(self, mock_sendto):
+        with self.measure.time('mymetric') as dimensions:
+            dimensions['name'] = 'john'
+
+        self.assertEqual(mock_sendto.call_count, 1)
+        message = json.loads(mock_sendto.call_args[0][0])
+        self.assertEqual(len(message), 6)
+        self.assertIn('client', message)
+        self.assertEqual(message['client'], 'myclient')
+        self.assertIn('metric', message)
+        self.assertEqual(message['metric'], 'mymetric')
+        self.assertIn('time', message)
+        self.assertIsInstance(message['time'], float)
+        self.assertGreater(message['time'], 0)
+        self.assertIn('error_type', message)
+        self.assertEqual(message['error_type'], '')
+        self.assertIn('error_value', message)
+        self.assertEqual(message['error_value'], '')
+        self.assertIn('name', message)
+        self.assertEqual(message['name'], 'john')
+
+    @patch('socket.socket.sendto')
+    def test_dimensions_must_not_override_parameters(self, mock_sendto):
+        with self.measure.time('mymetric') as dimensions:
+            dimensions['client'] = 'otherclient'
+            dimensions['metric'] = 'othermetric'
+            dimensions['time'] = -1
+            dimensions['error_type'] = 'othertype'
+            dimensions['error_value'] = 'othervalue'
+
+        self.assertEqual(mock_sendto.call_count, 1)
+        message = json.loads(mock_sendto.call_args[0][0])
+        self.assertEqual(len(message), 5)
+        self.assertIn('client', message)
+        self.assertEqual(message['client'], 'myclient')
+        self.assertIn('metric', message)
+        self.assertEqual(message['metric'], 'mymetric')
+        self.assertIn('time', message)
+        self.assertIsInstance(message['time'], float)
+        self.assertGreater(message['time'], 0)
+        self.assertIn('error_type', message)
+        self.assertEqual(message['error_type'], '')
+        self.assertIn('error_value', message)
+        self.assertEqual(message['error_value'], '')
+
+    @patch('socket.socket.sendto')
+    def test_must_not_change_dimensions_dict(self, mock_sendto):
+        with self.measure.time('mymetric') as dimensions:
+            dimensions = {}
+        self.assertFalse(dimensions)
+
+    @patch('socket.socket.sendto', side_effect=socket.error)
+    def test_must_not_throw_socket_exception(self, mock_sendto):
+        try:
+            with self.measure.time('mymetric'):
+                pass
+        except socket.error:
+            self.fail('socket.error raised from time')
+
+    @patch('socket.socket.sendto', side_effect=socket.error(80, 'error'))
+    @patch('measures.logger.error')
+    def test_must_log_socket_error(self, mock_warn, mock_sendto):
+        with self.measure.time('mymetric'):
+            pass
+        mock_warn.assert_called_once_with('Error on sendto. [Errno 80]')
+
+    @patch('socket.socket.sendto')
+    def test_must_send_packet_with_time_spent_on_error(self, mock_sendto):
+        with self.assertRaises(ValueError):
+            with self.measure.time('mymetric'):
+                raise ValueError('foo')
+
+        self.assertEqual(mock_sendto.call_count, 1)
+        message = json.loads(mock_sendto.call_args[0][0])
+        self.assertEqual(len(message), 5)
+        self.assertIn('client', message)
+        self.assertEqual(message['client'], 'myclient')
+        self.assertIn('metric', message)
+        self.assertEqual(message['metric'], 'mymetric')
+        self.assertIn('time', message)
+        self.assertIsInstance(message['time'], float)
+        self.assertGreater(message['time'], 0)
+        self.assertIn('error_type', message)
+        self.assertEqual(message['error_type'], str(ValueError))
+        self.assertIn('error_value', message)
+        self.assertEqual(message['error_value'], 'foo')
+
+    @patch('socket.socket.sendto')
+    def test_must_send_packet_with_dimensions_on_error(self, mock_sendto):
+        with self.assertRaises(ValueError):
+            with self.measure.time('mymetric') as dimensions:
+                dimensions['name'] = 'john'
+                raise ValueError('foo')
+
+        self.assertEqual(mock_sendto.call_count, 1)
+        message = json.loads(mock_sendto.call_args[0][0])
+        self.assertIn('name', message)
+        self.assertEqual(message['name'], 'john')
